c3e0834ea2c220e98b3a0281e505d928c64d00e9
Jason_Jan <1211241203@qq.com>
2018-11-15 22:53:16 +0800

    修改demo 使用定时器+随机值 模拟任务，依旧采用服务

--- ipcdemo0/src/main/java/com/example/jasonjan/ipcdemo/MainActivity.java
+++ ipcdemo0/src/main/java/com/example/jasonjan/ipcdemo/MainActivity.java
@@ -1,41 +1,20 @@
 package com.example.jasonjan.ipcdemo;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RemoteViews;
-import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
     private boolean mIsBind = false;
     private boolean mIsConnected = false;
     private MainService mMainService;
-    private final int NOTIFICATION_ID = 98;
-    private boolean mIsForegroundService = false;
-    private static final String APP1PACKGENAME="com.example.ipcdemo1";
-    private static final String APP1MAINACTIVITY="com.example.ipcdemo1.MainActivity";
-    private boolean isAways;//是否一直显示
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,90 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    /**
+     * 按钮：模拟任务
+     * @param v
+     */
+    public void timerTask(View v){
         if(null == startService(new Intent(MainActivity.this,MainService.class))){
             new Throwable("无法启动服务");
         }
         mIsBind = bindService(new Intent(MainActivity.this,MainService.class),mConnection, Context.BIND_AUTO_CREATE);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(mIsConnected && mIsForegroundService){
-            mMainService.stopForeground(true);
-            mIsForegroundService = false;
-        }
-        SharedPreferences share=getSharedPreferences("app",Context.MODE_PRIVATE);
-        isAways=share.getBoolean("isAlways",false);
-        if(isAways){
-            if(mIsBind && mMainService != null && !mIsForegroundService){
-                mMainService.startForeground(NOTIFICATION_ID,getNotification());
-                mIsForegroundService = true;
-            }
-        }
-    }
-
-    public void sendANotification(View v){
-        if(mIsBind && mMainService != null && !mIsForegroundService){
-            mMainService.startForeground(NOTIFICATION_ID,getNotification());
-            mIsForegroundService = true;
-        }
-    }
-
-    public void isAlways(View v){
-        if(isAways){
-            isAways=false;
-            SharedPreferences sharedPreferences = getSharedPreferences("app", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean("isAlways",isAways);
-            editor.apply();
-
-        }else{
-            isAways=true;
-            SharedPreferences sharedPreferences = getSharedPreferences("app", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean("isAlways",isAways);
-            editor.apply();
-            if(mIsBind && mMainService != null && !mIsForegroundService){
-                mMainService.startForeground(NOTIFICATION_ID,getNotification());
-                mIsForegroundService = true;
-            }
-        }
-    }
-
-    /**
-     * 产生一个前台通知
-     * @return
-     */
-    private Notification getNotification(){
-
-        RemoteViews remoteViews = new RemoteViews(this.getPackageName(), R.layout.view_notification_type_0);
-        //定义值为0时跳转到App0的逻辑
-        Intent intent0 = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent0=PendingIntent.getActivity(this,100,intent0,PendingIntent.FLAG_UPDATE_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.value0_btn,pendingIntent0);
-
-        //定义值为1时跳转到App1的逻辑
-        Intent intent1=new Intent(Intent.ACTION_MAIN);
-        ComponentName componentName = new ComponentName(APP1PACKGENAME, APP1MAINACTIVITY);
-        intent1.setComponent(componentName);
-        PendingIntent pendingIntent1=PendingIntent.getActivity(this,101,intent1,PendingIntent.FLAG_UPDATE_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.value1_btn,pendingIntent1);
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setOngoing(true);
-        builder.setPriority(NotificationCompat.PRIORITY_MAX);
-        builder.setSmallIcon(R.drawable.notification);
-
-        Notification notification = builder.build();
-        notification.contentView = remoteViews;
-
-        return notification;
-    }
 
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
--- ipcdemo0/src/main/java/com/example/jasonjan/ipcdemo/MainService.java
+++ ipcdemo0/src/main/java/com/example/jasonjan/ipcdemo/MainService.java
@@ -1,9 +1,17 @@
 package com.example.jasonjan.ipcdemo;
 
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * tips :服务
@@ -13,6 +21,9 @@ import android.os.IBinder;
  */
 public class MainService extends Service {
 
+    private Timer timer;//定时任务
+    private static final String APP1PACKGENAME="com.example.ipcdemo1";
+    private static final String APP1MAINACTIVITY="com.example.ipcdemo1.MainActivity";
     private ServiceHelp mHelper = new ServiceHelp();
 
     public class ServiceHelp extends Binder {
@@ -21,6 +32,30 @@ public class MainService extends Service {
         }
     }
 
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 0:
+                    Intent intent = new Intent(MainService.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    Toast.makeText(MainService.this,"跳转到APP0",Toast.LENGTH_SHORT).show();
+                   break;
+                case 1:
+                    ComponentName componentName=new ComponentName(APP1PACKGENAME,APP1MAINACTIVITY);
+                    Intent intent1=new Intent();
+                    intent1.setComponent(componentName);
+                    intent1.setAction("android.intent.action.VIEW");
+                    intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent1);
+                    Toast.makeText(MainService.this,"跳转到APP1",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -28,6 +63,7 @@ public class MainService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        doTask();
         return mHelper;
     }
 
@@ -50,6 +86,34 @@ public class MainService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if(timer!=null){
+            timer.cancel();
+        }
+    }
+
+    /**
+     * 开始任务
+     */
+    public void doTask(){
+        timer = new Timer(true);
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                //产生一个value
+                Random random = new Random();
+                int i = random.nextInt(1000);
+                int value=i%2;
+
+                //跳转逻辑
+                if(value==0){
+                    mHandler.sendEmptyMessage(0);
+                }else{
+                    mHandler.sendEmptyMessage(1);
+                }
+            }
+        };
+
+        timer.schedule(task,3000,3000);
     }
 
 }
